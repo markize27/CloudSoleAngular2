e5377ed752f098581f2e5deb30d0b9f663579bd9
markize27 <markize27@gmail.com>
2018-06-10 23:43:51 +0300

    e

--- src/main/webapp/resources/js/controllers/AddressBookController.js
+++ src/main/webapp/resources/js/controllers/AddressBookController.js
@@ -5,19 +5,19 @@
 
 var AddressBookController = function($scope, $http){
 
-    var http = require("http");
-    setInterval(function() {
-        http.get("http://<your app name>.herokuapp.com");
-    }, 300000); // every 5 minutes (300000)
+
 
 
     $scope.editMode = false;
     $scope.position = '';
-
+    setInterval(function() {
+        $http.get("https://boiling-dawn-93304.herokuapp.com/");
+    }, 300000); // every 5 minutes (300000)
     $scope.viewAllAddressBook = function(){
         $http.get('address/all.json').success(function(response){
             $scope.addressBooks = response;
         })
+
     }
 
     $scope.resetAddressBookField = function(){
